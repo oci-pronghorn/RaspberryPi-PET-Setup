42aecc69665a1855ee9ef25b13cbb64849279782
Nathan Tippy <nathan@tippy.name>
2016-08-15 21:35:21 -0500

    update to use 0.0.2 of architype

--- setup.sh
+++ setup.sh
@@ -1,19 +1,15 @@
 #!/usr/bin/bash
 
-#sudo apt-get update
+sudo apt-get update
 
-#install maven
-#sudo apt-get --assume-yes install maven
+install maven
+sudo apt-get --assume-yes install maven
 
 #copy workspace
-#cp -R javaLessons ~/javaLessons
+cp -R javaLessons ~/javaLessons
 
-#project contains 'settings.xml'.  The repositories found within should be place
-#inside the ~\.m2\settings.xml file. The steps below however preload the jars and 
-#makes that superfluous
-
-#hook into the sysem once and we have all the needed resources
-mvn -s ./settings.xml archetype:generate -DarchetypeGroupId=com.ociweb.iot.archetype -DarchetypeArtifactId=PronghornIoT-Archetype -DarchetypeVersion=0.0.1 -DgroupId=com.dexterindustries.grovepi.project -DartifactId=setupValidator -Dversion=1.0-SNAPSHOT -Dpackage=com.dexterindustries.grovepi.project.setupValidator -DinteractiveMode=false -DarchetypeRepository=https://repository-pronghorn.forge.cloudbees.com/release
+#hook into the system once and we have all the needed resources
+mvn -s ./settings.xml archetype:generate -DarchetypeGroupId=com.ociweb.iot.archetype -DarchetypeArtifactId=PronghornIoT-Archetype -DarchetypeVersion=0.0.2 -DgroupId=com.dexterindustries.grovepi.project -DartifactId=setupValidator -Dversion=1.0-SNAPSHOT -Dpackage=com.dexterindustries.grovepi.project.setupValidator -DinteractiveMode=false -DarchetypeRepository=https://repository-pronghorn.forge.cloudbees.com/release
 
 
 cd setupValidator
