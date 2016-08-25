1561ac0c5a5a85e55841bd8d47802c7127b64647
Nathan Tippy <nathan@tippy.name>
2016-08-25 14:01:50 -0500

    build offline and removed default project

--- geany_mvn_conf.py
+++ geany_mvn_conf.py
@@ -13,20 +13,19 @@ config.optionxform = str
 
 config.add_section('project')
 config.set('project','project_file_path','/home/pi/javaLessons/answers')
-config.set('project','session_file','/home/pi/javaLessons/answers/001-LightUsingButton/001-LightUsingButton.geany')
 
 config.add_section('build-menu')
 config.set('build-menu','NF_00_LB','_Compile')
-config.set('build-menu','NF_00_CM','mvn compile')
+config.set('build-menu','NF_00_CM','mvn compile --offline')
 config.set('build-menu','NF_00_WD','%p')
 config.set('build-menu','NF_01_LB','_Other targets')
 config.set('build-menu','NF_01_CM','mvn')
 config.set('build-menu','NF_01_WD','%p')
 config.set('build-menu','NF_02_LB','_Test')
-config.set('build-menu','NF_02_CM','mvn test')
+config.set('build-menu','NF_02_CM','mvn test --offline')
 config.set('build-menu','NF_02_WD','%p')
 config.set('build-menu','NF_03_LB','_Install')
-config.set('build-menu','NF_03_CM','mvn install')
+config.set('build-menu','NF_03_CM','mvn install --offline')
 config.set('build-menu','NF_03_WD','%p')
 config.set('build-menu','error_regex','\[ERROR\] ([^:\[]+):?\[([0-9]+)')
 
